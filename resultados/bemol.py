e77c8b3ae4e6b4f65c7b07191ab7789e7f38a46d
Gustavo Bessa <gustavobessa2001@gmail.com>
2019-09-04 02:26:49 -0400

    Add files via upload

--- resultados/bemol.py
+++ resultados/bemol.py
@@ -4,8 +4,8 @@ import pandas as pd
 ind = np.arange(12)
 
 # # # IMPORT DADOS EXCEL # # #
-d1_xls = pd.read_excel('dados.xlsx', 'realizado', index= False)
-d2_xls = pd.read_excel('dados.xlsx', 'orcado', index= False)
+d1_xls = pd.read_excel('dados.xlsx', 'realizado', index= False) # Plan. 1
+d2_xls = pd.read_excel('dados.xlsx', 'orcado', index= False) # Plan. 2
 
 
 # # # TRATAMENTO PLANILHA 1 # # #
@@ -22,10 +22,20 @@ d2_xls.columns = ['mesO', 'orcado']  # Renomeando colunas de d2_xls
 df = d1_xls.merge(d2_xls, left_on='mes', right_on='mesO') # Merge entre PLAN1 e PLAN2
 df = df.drop('mesO', axis = 1) # Apagando coluna mesO (repetida)
 
+# # # DIFERENCA REALIZADO x ORCADO # # #
+df['diferenca'] = 0 # Criando uma nova coluna
+df['diferenca'] = df['orcado'] - df['realizado']  # Calculando Diferenca
+
 # # # DEBUG # # #
+print('Realizado:')
 print(d1_xls.head())
+print('------')
+print('Orcado:')
 print(d2_xls.head())
+print('------')
+print('Resultado(Merge)')
 print(df.head())
+print('------')
 
 # # # CSV # # #
 df.to_csv('dados_orcamento.csv', encoding='utf-8', index=False) # CSV
@@ -40,4 +50,4 @@ plt.xlabel('Meses') # Nome eixo x
 plt.ylabel('R$') # Nome eixo y
 plt.legend((p1[0], p2[0]), ('Orcado', 'Realizado')) # Legendas
 plt.savefig('grafico_orcamento.png', transparent = False) # Salvar em arquivo
-plt.show()
+plt.show() # Exibir Grafico
